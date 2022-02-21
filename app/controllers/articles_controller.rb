6bcc0f72c8c0195da5cdccec403695dabe705170
rajverma-kiwi <raj.verma@kiwitech.com>
2022-02-21 16:50:25 +0530

    Delete articles options

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 
     end
     def index 
-        
+
         @articles = Article.all
 
     end
@@ -45,6 +45,12 @@ class ArticlesController < ApplicationController
             render 'edit'
 
         end
+
+    end
+    def destroy
+         @article = Article.find(params[:id])
+         @article.destroy
+         redirect_to articles_path
 
     end
 
