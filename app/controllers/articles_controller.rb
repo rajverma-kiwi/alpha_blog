fe6ffc7da57abceeca18a2804d4b4e7662d1011c
rajverma-kiwi <raj.verma@kiwitech.com>
2022-02-21 16:35:47 +0530

    Working with Edit and Update (implemented)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,23 @@ class ArticlesController < ApplicationController
 
     end
     def index 
-
         
         @articles = Article.all
+
     end
 
     def new
 
         @article = Article.new
     end
+
+    def edit 
+
+        @article = Article.find(params[:id])
+
+    end
+
+
     def create
         #render plain: params[:article]
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -26,6 +34,17 @@ class ArticlesController < ApplicationController
             render 'new'
         end
         #render plain: @article.inspect
+
+    end
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+
+        end
 
     end
 
