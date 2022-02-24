7c6218457cb0d5ac34d4266f0b928879bab9245b
rajverma-kiwi <raj.verma@kiwitech.com>
2022-02-24 16:44:37 +0530

    Add one- yo -many association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article]
         @article = Article.new(article_params)
+        @article.user = User.first
         #@article.save
         #redirect_to article_path(@article)
         if @article.save
