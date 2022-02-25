a98b3c884a9ed80da7e652fd7c9c853584aab13a
rajverma-kiwi <raj.verma@kiwitech.com>
2022-02-25 10:48:18 +0530

    Users-signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :edit, :create, :update, :destroy]
+  resources :articles #only: [:show, :index, :new, :edit, :create, :update, :destroy]
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
